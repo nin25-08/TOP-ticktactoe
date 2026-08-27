0cb14952e029b54d61955bd19aec065557057918
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-08-27 14:58:54 +0200

    created grid model for interface and communication layer from visual grid to board Object, created handle gameOver boilerplate logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
+const body = document.querySelector("body")
+const grid = document.querySelector(".grid")
+const pname = document.querySelector(".player-name")
+const dialog = document.querySelector("dialog");
+
+
 const board = (() => {
     let gameBoard =
         [["", "", ""],
@@ -12,30 +18,28 @@ const board = (() => {
         if (row < 3 && row >= 0 &&
             col < 3 && col >= 0) {
             gameBoard[row][col] = value;
+            return isGameOver(value);
         }
         else {
             return "invalidMove";
         }
     })
-
     const isGameOver = ((value) => {
-        let rows=[[gameBoard[0][0],gameBoard[0][1],gameBoard[0][2]],[gameBoard[1][0],gameBoard[1][1],gameBoard[1][2]],[gameBoard[2][0],gameBoard[2][1],gameBoard[2][2]]]
-        let cols=[[gameBoard[0][0],gameBoard[1][0],gameBoard[2][0]],[gameBoard[0][1],gameBoard[1][1],gameBoard[2][1]],[gameBoard[0][2],gameBoard[1][2],gameBoard[2][2]]]
-        let diag=[[gameBoard[0][0],gameBoard[1][1],gameBoard[2][2]],[gameBoard[0][2],gameBoard[1][1],gameBoard[2][0]]]
+        let rows = [[gameBoard[0][0], gameBoard[0][1], gameBoard[0][2]], [gameBoard[1][0], gameBoard[1][1], gameBoard[1][2]], [gameBoard[2][0], gameBoard[2][1], gameBoard[2][2]]]
+        let cols = [[gameBoard[0][0], gameBoard[1][0], gameBoard[2][0]], [gameBoard[0][1], gameBoard[1][1], gameBoard[2][1]], [gameBoard[0][2], gameBoard[1][2], gameBoard[2][2]]]
+        let diag = [[gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]], [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]]]
 
-
-        return evaluateLine(rows,value)||evaluateLine(cols,value)||evaluateLine(diag,value)
+        return evaluateLine(rows, value) || evaluateLine(cols, value) || evaluateLine(diag, value)
     })
-      const evaluateLine = ((line,value) => {
-        for(let i=0;i<line.length;i++){
-            let count=0;
-            for(let j=0;j<line[i].length;j++){
-                if(line[i][j]==value){
+    const evaluateLine = ((line, value) => {
+        for (let i = 0; i < line.length; i++) {
+            let count = 0;
+            for (let j = 0; j < line[i].length; j++) {
+                if (line[i][j] == value) {
                     count++;
-                    
                 }
             }
-            if(count==3){
+            if (count == 3) {
                 return true;
             }
         }
@@ -48,15 +52,74 @@ const board = (() => {
         }
     })
 
-    return { getBoard, makeMove, printBoard,isGameOver}
+    const clearBoard = (() => {
+        gameBoard =
+            [["","",""],
+            ["", "",""],
+            ["","",""]];
+
+    })
+
+    return { getBoard, makeMove, printBoard, clearBoard, isGameOver}
 })();
 
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+}
 
-board.makeMove("O",0,0)
-board.makeMove("O",0,1)
-board.makeMove("O",0,2)
-console.log(board.printBoard())
 
-console.log(board.printBoard())
 
-console.log(board.isGameOver("O"))
+function displayBoard() {
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement("div");
+        row.className="row"
+        for (let j = 0; j < 3; j++) {
+            const elem = document.createElement("div");
+            elem.className="square"
+            elem.dataset.row = i
+            elem.dataset.col=j
+            row.appendChild(elem)
+        }
+        grid.appendChild(row)
+    }
+}
+const createPlayerHandler = ((player1,player2)=>{
+    let currentPlayer=player1;
+    const getCurrentPlayer = ()=>{
+        return currentPlayer;
+    }
+    const togglePlayer = ()=>{
+        if(currentPlayer==player1){
+            currentPlayer=player2
+        }else{
+            currentPlayer=player1
+        }
+    }
+    return {getCurrentPlayer,togglePlayer}
+})
+
+const player1=new Player("Jhon","X")
+const player2=new Player("Lebron","O")
+
+const playerHandler=createPlayerHandler(player1,player2)
+body.addEventListener("click",(e)=>{
+    if(e.target.className=="square"){
+        const currentPlayer=playerHandler.getCurrentPlayer()
+        board.makeMove(currentPlayer.marker,e.target.dataset.row,e.target.dataset.col)
+        e.target.textContent=currentPlayer.marker
+
+        if(board.isGameOver(currentPlayer.marker)){
+            handleGameOver(currentPlayer)
+        }else{
+        playerHandler.togglePlayer()
+        board.printBoard()
+        }
+    }
+})
+function handleGameOver(player){
+    console.log("gameOver")
+    pname.textContent=player.name
+    dialog.show()
+}
+displayBoard();
